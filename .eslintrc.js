980dab8d399ee2a3e460c84578acdcf381c259ba
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-09-07 09:13:56 +0700

    resolving webpack.config.js path

--- .eslintrc.js
+++ .eslintrc.js
@@ -65,7 +65,7 @@ module.exports = {
   "settings": {
     "import/resolver": {
       "webpack": {
-        "config": "./internals/webpack/webpack.config.js"
+        "config": "./webpack.config.js"
       }
     }
   }
