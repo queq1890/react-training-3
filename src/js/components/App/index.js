e15ec484813a197a917a0413874290ca19996253
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-09-10 18:46:42 +0700

    Render AppContainer and pass the locale props to App component

--- src/js/components/App/index.js
+++ src/js/components/App/index.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import React, { PureComponent } from 'react';
 import ListContainer from 'containers/ListContainer';
 import FormContainer from 'containers/FormContainer';
+import { addLocaleData, FormattedMessage, IntlProvider } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import ja from 'react-intl/locale-data/ja';
+import messages from 'locales/messages';
 import MainWrapper from './MainWrapper';
 
-const App = () => (
-  <MainWrapper>
-    <div className="col-md-4 offset-md-1">
-      <h2>
-        <FormattedMessage id="articles.header" />
-      </h2>
-      <ListContainer />
-    </div>
-    <div className="col-md-4 offset-md-1">
-      <h2>
-        <FormattedMessage id="article.new" />
-      </h2>
-      <FormContainer />
-    </div>
-  </MainWrapper>
-);
+addLocaleData([...en, ...ja]);
+
+class App extends PureComponent {
+  render() {
+    const { locale } = this.props;
+    return (
+      <IntlProvider locale={locale} messages={messages[locale]}>
+        <MainWrapper>
+          <div className="col-md-4 offset-md-1">
+            <h2>
+              <FormattedMessage id="articles.header" />
+            </h2>
+            <ListContainer />
+          </div>
+          <div className="col-md-4 offset-md-1">
+            <h2>
+              <FormattedMessage id="article.new" />
+            </h2>
+            <FormContainer />
+          </div>
+        </MainWrapper>
+      </IntlProvider>
+    );
+  }
+}
 
 export default App;
