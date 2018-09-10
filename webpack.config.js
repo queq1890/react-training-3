d59890edcd9e4fe30d3679e75c89e85f595b0b5f
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-09-10 18:44:50 +0700

    create locale file

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,7 @@ module.exports = {
     alias: {
       components: path.join(__dirname, 'src/js/components'),
       containers: path.join(__dirname, 'src/js/containers'),
+      locales: path.join(__dirname, 'src/js/locales'),
     },
     extensions: ['.webpack.js', '.web.js', '.js', '.yml', '.style.js'],
   },
