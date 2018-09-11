28ce20239694fc36156505c5f86b2e4127f17e36
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-09-11 18:53:39 +0700

    add reducer specs

--- src/tests/containers/FormContainer/reducer.spec.js
+++ src/tests/containers/FormContainer/reducer.spec.js
@@ -3,10 +3,22 @@ import { ADD_ARTICLE } from 'containers/FormContainer/constants';
 
 describe('formReducer', () => {
   it('should return the initial state', () => {
-    expect(formReducer(undefined, {})).toEqual([
-      {
-        articles: [],
-      },
-    ]);
+    expect(formReducer(undefined, {})).toEqual({
+      articles: [],
+    });
+  });
+
+  it('should handle ADD_ARTICLE', () => {
+    const action = { type: ADD_ARTICLE, payload: { title: 'hoge', id: '1' } };
+    const expectedState = {
+      articles: [
+        {
+          title: 'hoge',
+          id: '1',
+        },
+      ],
+    };
+
+    expect(formReducer(undefined, action)).toEqual(expectedState);
   });
 });
