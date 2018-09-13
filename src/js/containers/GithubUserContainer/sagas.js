33fe540a159cf302cf41dc480a80249aacb78c5b
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-09-13 11:09:12 +0700

    stash

--- src/js/containers/GithubUserContainer/sagas.js
+++ src/js/containers/GithubUserContainer/sagas.js
@@ -1,1 +1,17 @@
-// https://api.github.com/users/queq1890
+import {
+  call, put, fork, takeLatest, all,
+} from 'redux-saga/effects';
+import axios from 'axios';
+import FETCH_GITHUB_USER from './constants';
+
+const getGithubUser = () => {
+  const user = axios.get('https://api.github.com/users/queq1890');
+  console.log(user);
+  return user;
+};
+
+function* fetchGithubUser(action) {}
+
+export default function* githubSaga() {
+  yield takeEvery(FETCH_GITHUB_USER, fetchGithubUser);
+}
