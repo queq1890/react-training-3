c558f7a98785decfa14c85d22b1dc3abb00358a2
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-09-13 09:08:32 +0700

    fundamental settings to use redux-saga

--- src/js/store.js
+++ src/js/store.js
@@ -1,6 +1,13 @@
-import { createStore } from 'redux';
+import 'regenerator-runtime/runtime';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers';
+import rootSaga from './sagas';
 
-const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+// After we define rootSaga, remove comment out
+sagaMiddleware.run(rootSaga);
 
 export default store;
