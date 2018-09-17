97a2c9121b8ad8ee62fa26691d4c538872468da2
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-09-17 10:10:00 +0700

    change request url

--- src/js/containers/GithubUserContainer/requests.js
+++ src/js/containers/GithubUserContainer/requests.js
@@ -1,5 +1,3 @@
 import axios from 'axios';
 
-export const getUserAPI = () => axios
-  .get('https://jsonplaceholder.typicode.com/users/1')
-  .then(res => res.data);
+export const getUserAPI = () => axios.get('https://api.github.com/users/queq1890').then(res => res.data);
