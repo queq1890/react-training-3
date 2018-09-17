a45710ae139fb439e3fe9f44bcf3c6b601f086dc
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-09-17 10:11:16 +0700

    Show Github avatar url if it's not empty

--- src/js/components/GithubUser/index.js
+++ src/js/components/GithubUser/index.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import isEmpty from 'lodash/isEmpty';
 
 class GithubUser extends PureComponent {
   getProfile = () => {
@@ -8,9 +9,14 @@ class GithubUser extends PureComponent {
   };
 
   render() {
-    const { profile, err } = this.props;
-    console.log('from render profile', profile);
+    const { profile, err, loading } = this.props;
     console.log('from render error', err);
+    console.log('from render profile', profile);
+    console.log('loading', loading);
+    console.log('isEmpty', isEmpty(profile));
+    if (!isEmpty(profile)) {
+      return <Img src={profile.avatar_url} alt="github icon" />;
+    }
     return (
       <div>
         <button type="submit" onClick={this.getProfile}>
@@ -25,6 +31,7 @@ GithubUser.propTypes = {
   fetchGithubUser: PropTypes.func,
   profile: PropTypes.arrayOf(Object),
   err: PropTypes.bool,
+  isRequested: PropTypes.bool,
 };
 
 export default GithubUser;
